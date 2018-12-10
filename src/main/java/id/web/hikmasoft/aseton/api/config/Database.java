b12cadfc7feb70bd4c21278eb84b48ddeaa00772
Hendra Dwi Saputra <babeguru@gmail.com>
2018-12-10 23:45:14 +0700

    Konfigurasi untuk basisdata postgresql dengan env DATABASE_URL

---
+++ src/main/java/id/web/hikmasoft/aseton/api/config/Database.java
@@ -0,0 +1,30 @@
+package id.web.hikmasoft.aseton.api.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@ComponentScan(basePackages = "id.web.hikmasoft.aseton.api")
+public class Database {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
